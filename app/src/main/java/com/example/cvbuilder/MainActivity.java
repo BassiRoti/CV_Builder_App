6b95546ad5858ef14684d13185fbc28be7a21b4c
Abbas Rizvi <abbasriz1234567890@gmail.com>
2025-03-13 02:38:39 +0500

    Splash Screen

---
+++ app/src/main/java/com/example/cvbuilder/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.cvbuilder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+    Animation anim;
+
+
+
+    private void init(){
+        logo=findViewById(R.id.logo);
+        anim= AnimationUtils.loadAnimation(this,R.anim.logo_animate);
+        logo.setAnimation(anim);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+
+        new Handler()
+                .postDelayed(()->{
+                    Intent i=new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(i);
+                    finish();
+                },3000);
+    }
+}
